7e1cfea49d876861783fad10e770965f5a53bdac
bigmacaron <semin8890@gmail.com>
2020-06-23 09:22:05 +0900

    add to host ip

--- app.py
+++ app.py
@@ -92,4 +92,4 @@ def get_context_data(self, **kwargs):
     return context
 
 if __name__ == '__main__':
-    app.run(debug=True)  # , host="0.0.0.0", port="5114"
+    app.run(debug=True, host="0.0.0.0")  # , , port="5114"
