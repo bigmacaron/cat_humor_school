cfa5a5713998b2eea7484ccac440ff1c6b8c8647
Kwon-Jongbeom <bolk9652@naver.com>
2020-06-23 17:14:33 +0900

    edit crowler
    board crowler...

--- dao/site_infomation.py
+++ dao/site_infomation.py
@@ -77,3 +77,40 @@ INVEN = {
     'board_titles':None, 
     'board_links':None
     }
+
+if __name__ == '__main__':
+
+    import requests
+    from bs4 import BeautifulSoup
+
+    user_agent = "Mozilla/5.0 (Linux; Android 8.1.0; Moto G (5S) Plus) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.111 Mobile Safari/537.36"
+
+    def html(site):
+        return requests.get(site['board_link'], headers={'User-Agent':user_agent}).text
+
+    # [ board ]
+
+    # # INVEN
+    # soup = BeautifulSoup(html(INVEN), features='html.parser')
+    # titles = [tag.strong.next_sibling.strip() for tag in soup.select('.subject > .title')]
+    # print(titles)
+    # links = [tag['href'] for tag in soup.select('.subject')]
+
+    # # ETOLAND
+    # soup = BeautifulSoup(html(ETOLAND), features='html.parser')
+    # titles = [tag.a.div.font.next_sibling.strip() for tag in soup.select('.subject')]
+    # links = [tag['href'] for tag in soup.select('.subject > a')]
+
+    # # HUMORUNIV
+    # def html2(site):
+    #     html = requests.get(site['board_link'], headers={'User-Agent':user_agent})
+    #     html.encoding = 'euc-kr'
+    #     return html.text
+    # soup = BeautifulSoup(html2(HUMORUNIV), features='html.parser')
+    # titles = [tag.text for tag in soup.select('span[id*="title_chk_pds"]')]
+    # links = [tag['href'] for tag in soup.select('.list_body_href')]
+
+    # # YGOSU
+    # soup = BeautifulSoup(html(YGOSU), features='html.parser')
+    # titles = [tag.next_sibling.strip() for tag in soup.select('.category')]
+    # links = [tag['href'] for tag in soup.select('.li2 > li > a')]
